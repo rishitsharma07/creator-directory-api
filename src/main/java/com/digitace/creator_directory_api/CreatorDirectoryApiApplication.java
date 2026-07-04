c9affc3f1c3da42ecb88344600d727b7ab965e0a
rishitsharma07 <rishitsharma1675@gmail.com>
2026-07-04 19:07:24 +0530

    Initial project setup with PostgreSQL

---
+++ src/main/java/com/digitace/creator_directory_api/CreatorDirectoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.digitace.creator_directory_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorDirectoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorDirectoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitace/creator_directory_api/CreatorDirectoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitace.creator_directory_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorDirectoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
