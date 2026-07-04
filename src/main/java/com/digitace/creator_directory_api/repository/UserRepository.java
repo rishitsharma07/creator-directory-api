96646667cb0314ee702aea73fe6abc4b3aa1e55a
KKumarPro <ak23071977@gmail.com>
2026-07-04 22:19:09 +0530

    feat: implement tenant gatekeeper, user repository, and request interceptor

---
+++ src/main/java/com/digitace/creator_directory_api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.digitace.creator_directory_api.repository;
+
+import com.digitace.creator_directory_api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
